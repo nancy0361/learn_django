994ccaf6ba06f4e14baff97c75fb25f7800f4043
Naixin <naixin.yu@duke.edu>
2019-01-19 10:35:50 -0500

    finish toturial 2,3

--- djangoProject/polls/urls.py
+++ djangoProject/polls/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     # kwargs: Arbitrary keyword arguments can be passed in a dictionary to the target view
     # name: give your url a name
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
